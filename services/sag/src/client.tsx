3400bc12f60ab8552b73d4686011977750e89c6a
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2026-05-13 21:25:06 -0700

    fix(sag): prevent first paint flash

--- services/sag/src/client.tsx
+++ services/sag/src/client.tsx
@@ -1,7 +1,6 @@
 import { StartClient } from '@tanstack/react-start/client'
 import { StrictMode, startTransition } from 'react'
 import { hydrateRoot } from 'react-dom/client'
-import './app/styles/app.css'
 
 startTransition(() => {
   hydrateRoot(
