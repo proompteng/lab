b96e492d46ed883db4e786be8d3ff096dc0175c8
Greg Konush <greg@gregko.com>
2026-07-30 04:22:16 +0000

    chore(bayn): remove rejected Candidate 16 artifacts

--- services/bayn/src/candidates/candidate-16-macro-breadth-regime/run.ts
+++
@@ -1,6 +0,0 @@
-import { NodeRuntime } from '@effect/platform-node'
-
-import { runCandidateDevelopmentCommand } from '../../candidate-development-command'
-import { candidateDevelopmentProgram } from './program'
-
-NodeRuntime.runMain(runCandidateDevelopmentCommand(candidateDevelopmentProgram))
