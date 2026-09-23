00f733e7a7f298ca7aee30582b971d1bc035503c
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2026-09-22 18:37:28 -0700

    fix(bayn): recover untouched cycles during open session (#14610)

--- services/bayn/src/db/migrations.ts
+++ services/bayn/src/db/migrations.ts
@@ -6,6 +6,7 @@ import jevBatches from '../../migrations/0078_jev_batches'
 import jevNativeStrategy from '../../migrations/0079_jev_native_strategy'
 import jevExitCommitDeadline from '../../migrations/0080_jev_exit_commit_deadline'
 import measuredSimulationClock from '../../migrations/0081_measured_simulation_clock'
+import openSessionFailureRearm from '../../migrations/0082_open_session_failure_rearm'
 import recoverIntradayAuthorityCycle from '../../migrations/0071_recover_intraday_authority_cycle'
 import intradayZeroFillRearm from '../../migrations/0072_intraday_zero_fill_rearm'
 import pacedIntradayEntryAttempts from '../../migrations/0073_paced_intraday_entry_attempts'
@@ -83,6 +84,7 @@ import brokerFeeAccounting from '../../migrations/0063_broker_fee_accounting'
 import partialIocCompletion from '../../migrations/0064_partial_ioc_completion'
 
 export const migrationLoader = PgMigrator.fromRecord({
+  '82_open_session_failure_rearm': openSessionFailureRearm,
   '81_measured_simulation_clock': measuredSimulationClock,
   '80_jev_exit_commit_deadline': jevExitCommitDeadline,
   '79_jev_native_strategy': jevNativeStrategy,
